d080b184b7c72186b6dd9193d1291908ccb8988e
harmreuben <harmreuben@gmail.com>
2026-07-17 19:40:51 +0300

    Update app configurations

--- backend/apps/users/apps.py
+++ backend/apps/users/apps.py
@@ -1,5 +1,6 @@
+# apps/users/apps.py
 from django.apps import AppConfig
 
-
 class UsersConfig(AppConfig):
-    name = 'apps.users'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.users'  # ← must match INSTALLED_APPS entry
